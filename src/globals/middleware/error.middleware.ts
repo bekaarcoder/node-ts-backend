be8d491ad812744fd0dd0e8af844ed1b900da607
bekaarcoder <s_shashank@outlook.com>
2024-12-12 22:35:17 +0530

    handle custom error

---
+++ src/globals/middleware/error.middleware.ts
@@ -0,0 +1,63 @@
+import { HTTP_STATUS } from '../constants/http';
+
+export abstract class CustomError extends Error {
+    abstract status: string;
+    abstract statusCode: number;
+
+    constructor(message: string) {
+        super(message);
+    }
+
+    public getErrorResponse() {
+        return {
+            status: this.status,
+            statusCode: this.statusCode,
+            message: this.message,
+        };
+    }
+}
+
+export class BadRequestException extends CustomError {
+    status: string = 'error';
+    statusCode: number = HTTP_STATUS.BAD_REQUEST;
+
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class UnAuthorizedException extends CustomError {
+    status: string = 'error';
+    statusCode: number = HTTP_STATUS.UNAUTHORIZED;
+
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class NotFoundException extends CustomError {
+    status: string = 'error';
+    statusCode: number = HTTP_STATUS.NOT_FOUND;
+
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class ForbiddenException extends CustomError {
+    status: string = 'error';
+    statusCode: number = HTTP_STATUS.FORBIDDEN;
+
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class InternalServerException extends CustomError {
+    status: string = 'error';
+    statusCode: number = HTTP_STATUS.INTERNAL_SERVER_ERROR;
+
+    constructor(message: string) {
+        super(message);
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
 import 'dotenv/config';
 import appRoutes from './globals/routes/appRoutes';
 import { HTTP_STATUS } from './globals/constants/http';
+import { CustomError } from './globals/middleware/error.middleware';
 
 class Server {
     private app: Application;
@@ -26,11 +27,23 @@ class Server {
     }
 
     private setupGlobalError(): void {
+        // Not Found Error
         this.app.all('*', (req, res, next) => {
             res.status(HTTP_STATUS.NOT_FOUND).json({
                 message: `URL ${req.originalUrl} not found`,
             });
         });
+
+        // Global Error
+        this.app.use(
+            (error: any, req: Request, res: Response, next: NextFunction) => {
+                if (error instanceof CustomError) {
+                    res.status(error.statusCode).json(error.getErrorResponse());
+                    return;
+                }
+                next();
+            }
+        );
     }
 
     private startServer() {
